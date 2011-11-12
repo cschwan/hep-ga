2018457a307e821596db5ed26b42305d54c41fdf
Christopher Schwan <schwan@uni-mainz.de>
2011-11-12 15:54:18 +0100

    Initial commit with autotools framework

---
+++ include/hep/ga.hpp
@@ -0,0 +1,6 @@
+#ifndef HEP_GA_HPP
+#define HEP_GA_HPP
+
+namespace hep {}
+
+#endif
---
+++ test/test_ga.cpp
@@ -0,0 +1,11 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MAIN
+#define BOOST_TEST_MODULE "Vegas Test"
+
+#include <hep/ga.hpp>
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(ga_test)
+{
+}
