d17dc1173f227fdd3fa0844764c5f6386dc6a00b
Christopher Schwan <schwan@uni-mainz.de>
2012-03-19 12:03:11 +0100

    Add grade(s)_to_list utilities
    - adds next_bit_permutation again and its test

---
+++ include/hep/utils/grade_to_list.hpp
@@ -0,0 +1,79 @@
+#ifndef HEP_UTILS_GRADE_TO_LIST_HPP
+#define HEP_UTILS_GRADE_TO_LIST_HPP
+
+/*
+ * hep-ga - An Efficient Numeric Template Library for Geometric Algebra
+ * Copyright (C) 2012  Christopher Schwan
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <hep/utils/list.hpp>
+#include <hep/utils/next_bit_permutation.hpp>
+
+namespace hep
+{
+
+/**
+ * \addtogroup list
+ * @{
+ */
+
+#define hep_first_component ((1 << grade) - 1)
+#define hep_last_component (hep_first_component << (A::dim() - grade))
+#define hep_next_component (next_bit_permutation(component))
+
+/**
+ * Returns the component list for \c grade in algebra \c A. Example:
+ * \code
+ * typedef hep::algebra<double, 3, 0> algebra;
+ * typedef hep::grade_to_list<algebra, 1>::type result;
+ * \endcode
+ * This is equivalent to the following definition:
+ * \code
+ * typedef hep::list<1,2,4> result;
+ * \endcode
+ */
+template <typename A, int grade, int component = hep_first_component,
+	int last_component = hep_last_component>
+struct grade_to_list
+{
+	static_assert (grade <= A::dim(), "grade bigger than algebra dimension");
+
+	/**
+	 * The result of this operation.
+	 */
+	typedef typename grade_to_list<A, grade, hep_next_component>::type::
+		template push_front<component>::type type;
+};
+
+#undef hep_next_component
+#undef hep_last_component
+#undef hep_first_component
+
+/// \cond DOXYGEN_IGNORE
+template <typename A, int grade, int component>
+struct grade_to_list<A, grade, component, component>
+{
+	typedef list<component> type;
+};
+/// \endcond
+
+/**
+ * @}
+ */
+
+}
+
+#endif
---
+++ include/hep/utils/grades_to_list.hpp
@@ -0,0 +1,73 @@
+#ifndef HEP_UTILS_GRADES_TO_LIST_HPP
+#define HEP_UTILS_GRADES_TO_LIST_HPP
+
+/*
+ * hep-ga - An Efficient Numeric Template Library for Geometric Algebra
+ * Copyright (C) 2012  Christopher Schwan
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <hep/utils/grade_to_list.hpp>
+#include <hep/utils/list.hpp>
+#include <hep/utils/merge.hpp>
+
+namespace hep
+{
+
+/**
+ * \addtogroup list
+ * @{
+ */
+
+/// \cond DOXYGEN_IGNORE
+template <typename A, int... grades>
+struct grades_to_list;
+
+template <typename A>
+struct grades_to_list<A>
+{
+	typedef list<> type;
+};
+/// \endcond
+
+/**
+ * Returns the component list for every grade specified  in \c grades in algebra
+ * \c A. Example:
+ * \code
+ * typedef hep::algebra<double, 3, 0> algebra;
+ * typedef hep::grade_to_list<algebra, 1,2>::type result;
+ * \endcode
+ * This is equivalent to the following definition:
+ * \code
+ * typedef hep::list<1,2,3,4,5,6> result;
+ * \endcode
+ */
+template <typename A, int grade1, int... grades>
+struct grades_to_list<A, grade1, grades...>
+{
+	/**
+	 * The result of this operation.
+	 */
+	typedef typename merge<typename grade_to_list<A, grade1>::type,
+		typename grades_to_list<A, grades...>::type>::type type;
+};
+
+/**
+ * @}
+ */
+
+}
+
+#endif
